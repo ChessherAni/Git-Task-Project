eec6651d717db405f6959718dac0a1e9f7b87101
Ani Chessher <annabel.chessher@hotmail.com>
2024-03-13 20:05:29 +0000

    Added comments to make code readable

--- helloWorld.py
+++ helloWorld.py
@@ -1,8 +1,13 @@
+# ask for user input
 user_message = input("What would you like to print: ")
 
+# check that the user didn't enter a newline or space
 while True:
     if user_message != "" and user_message != " ":
         break
+    # prompt user until valid input is given
     else:
         user_message = input("Input something that will be seen: ")
+
+# print user input
 print(user_message)
