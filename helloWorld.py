e93ff208e5f32b2d5c046835b755a00b48773168
Ani Chessher <annabel.chessher@hotmail.com>
2024-03-13 19:40:29 +0000

    Checked that program will print out a visible user input

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,8 @@
 user_message = input("What would you like to print: ")
+
+while True:
+    if user_message != "" and user_message != " ":
+        break
+    else:
+        user_message = input("Input something that will be seen: ")
 print(user_message)
